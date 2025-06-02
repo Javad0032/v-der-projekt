609bbd01c338a876fc2b39d447311f9eed1ed2b8
Javad0032 <javad0032@icloud.com>
2025-06-02 11:41:28 +0200

    feat: backend

--- backend/server.js
+++ backend/server.js
@@ -9,15 +9,27 @@ const app = express();
 const PORT = 5000;
 
 app.use(cors());
+//      "https://aviationweather.gov/api/data/metar",
 
 app.get("/api/data/metar", async (req, res) => {
+  //console.log(`Hämtar METAR-data för station: ${station}`);
   try {
-    res.send("working... data sent");
-    //hello
-  } catch {}
+    const response = await axios.get(
+      "http://api.weatherapi.com/v1/current.json?key=745d65f89f5a427998093630250206&q=London&aqi=no",
+      {
+        headers: {
+          "User-Agent": "Mozilla/5.0",
+        },
+      }
+    );
+
+    res.json(response.data);
+  } catch (error) {
+    console.error("Error fetching METAR-data:", error);
+    res.status(500).json({ error: "Could not fetch data" });
+  }
 });
 
 app.listen(PORT, () => {
   console.log(`🚀 Servern körs på http://localhost:${PORT}`);
 });
-
